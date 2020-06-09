ba7e9090e1dcd06db70bba8c0946eee6f4684e3e
mehash32 <65710141+mehash32@users.noreply.github.com>
2020-06-09 21:48:29 +0600

    Add files via upload

---
+++ frontend/src/data.js
@@ -0,0 +1,36 @@
+export default {
+    products: [
+    //     {
+    //     _id: '1',
+    //     name: 'Slim Shirt',
+    //     category: 'Shirts',
+    //     image:'/images/d1.jpg',
+    //     price: 1000,
+    //     brand: 'nike',
+    //     rating: 4.5,
+    //     numReviews: 10
+    // },
+    // {
+    //     _id: '2',
+    //     name: 'Fit Shirt',
+    //     category: 'Shirts',
+    //     image:'/images/d1.jpg',
+    //     price: 1000,
+    //     brand: 'nike',
+    //     rating: 4.5,
+    //     numReviews: 10
+    // },
+    // {
+    //     _id: '3',
+    //     name: 'Best Pant',
+    //     category: 'Pants',
+    //     image:'/images/d1.jpg',
+    //     price: 1000,
+    //     brand: 'nike',
+    //     rating: 4.5,
+    //     numReviews: 10
+    // },
+   
+]
+
+}
